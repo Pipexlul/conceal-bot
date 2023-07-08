8580189ce96d0f768bf5af986d3218a86b4c5c42
Pipexlul <felipgg@gmail.com>
2023-07-08 02:08:26 -0400

    Create main file app.ts. For now it only starts a websocket to Discord's API with the bot's token. It will gracefully shutdown when using CTRL + C

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,45 @@
-// TODO: Code!
+import { ActivityType } from "discord.js";
+import { SapphireClient } from "@sapphire/framework";
+
+import { botToken } from "./config/envConfig";
+
+const main = async () => {
+  const client = new SapphireClient({
+    intents: [
+      "GuildMessages",
+      "GuildMembers",
+      "GuildVoiceStates",
+      "MessageContent",
+    ],
+    presence: {
+      status: "online",
+      activities: [{ type: ActivityType.Playing, name: "with a spoon" }],
+    },
+  });
+
+  const terminateHandler = () => {
+    if (client.token !== null) {
+      console.log("Shutting down...");
+      client.destroy();
+    }
+  };
+
+  process.on("SIGKILL", terminateHandler);
+  process.on("SIGTERM", terminateHandler);
+  process.on("SIGINT", terminateHandler);
+  process.on("exit", terminateHandler);
+
+  const terminateEvents: string[] = ["SIGKILL", "SIGTERM", "SIGINT", "exit"];
+
+  terminateEvents.forEach((ev) => {
+    process.on(ev, terminateHandler);
+  });
+
+  client.once("ready", () => {
+    console.log(`${client.user?.tag} is ready!`);
+  });
+
+  await client.login(botToken);
+};
+
+main();
