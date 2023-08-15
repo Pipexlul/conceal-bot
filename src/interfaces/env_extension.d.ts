2aa9d1c1340a376825a3c5ee7ee4ddbf1dc0591f
Pipexlul <felipgg@gmail.com>
2023-08-15 01:19:13 -0400

    chore: edit and add new values to env extension .d.ts

--- src/interfaces/env_extension.d.ts
+++ src/interfaces/env_extension.d.ts
@@ -1,9 +1,12 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    readonly NODE_ENV: "development" | "production" | "test";
-    readonly PORT: string;
+    readonly NODE_ENV?: string;
+    readonly PORT?: string;
 
-    readonly ELEVENLABS_API_KEY: string;
-    readonly INWORLD_API_KEY: string;
+    readonly DISCORD_BOT_TOKEN?: string;
+    readonly TEST_SERVER_ID?: string;
+
+    readonly ELEVENLABS_API_KEY?: string;
+    readonly INWORLD_API_KEY?: string;
   }
 }
