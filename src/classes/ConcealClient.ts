6284d1692cba768a52b9e4d6ee30e391f9f23f75
Pipexlul <felipgg@gmail.com>
2023-08-22 18:01:38 -0400

    chore: change rootData path to src/ when running in dev mode

--- src/classes/ConcealClient.ts
+++ src/classes/ConcealClient.ts
@@ -1,7 +1,10 @@
+import path from "path";
 import { SapphireClient } from "@sapphire/framework";
+import { getRootData } from "@sapphire/pieces";
 import type { ClientOptions } from "discord.js";
 import type { ConcealClientOptions } from "../interfaces/ConcealClientOptions";
-import { getRootData } from "@sapphire/pieces";
+
+import { isDev } from "../utils/envUtils";
 
 class ConcealClient extends SapphireClient {
   private rootData = getRootData();
@@ -13,6 +16,11 @@ class ConcealClient extends SapphireClient {
   ) {
     super(saphOptions);
     // TODO: Change store paths here if needed
+    if (isDev) {
+      this.rootData.root = path.join(process.cwd(), "src");
+
+      console.log("Running in Dev mode, changed root to:", this.rootData.root);
+    }
 
     this.customOptions = options;
   }
