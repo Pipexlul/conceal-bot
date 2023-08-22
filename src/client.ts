86c4cc210c5ecb926d43fd6554bcaf24940ad55c
Pipexlul <felipgg@gmail.com>
2023-08-22 17:46:03 -0400

    chore: explicitly set log level to debug for now

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,14 @@
+import { LogLevel } from "@sapphire/framework";
 import ConcealClient from "./classes/ConcealClient";
 
 import { intents } from "./config/gatewayIntents";
 
 const client = new ConcealClient({
   intents: intents,
-  loadMessageCommandListeners: true
+  loadMessageCommandListeners: true,
+  logger: {
+    level: LogLevel.Debug
+  }
 });
 
 export default client;
