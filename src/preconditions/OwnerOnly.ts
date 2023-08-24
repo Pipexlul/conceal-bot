c2b22068470f1b4aeaae899babb2e2927238cc99
Pipexlul <felipgg@gmail.com>
2023-08-24 03:13:26 -0400

    chore: add owner only precondition

---
+++ src/preconditions/OwnerOnly.ts
@@ -0,0 +1,33 @@
+import { Precondition } from "@sapphire/framework";
+import type {
+  CommandInteraction,
+  ContextMenuCommandInteraction,
+  Message
+} from "discord.js";
+
+import envConfig from "../config/env";
+const { ownerId } = envConfig;
+
+export class OwnerOnlyPrecondition extends Precondition {
+  public override async messageRun(message: Message) {
+    return this.checkOwner(message.author.id);
+  }
+
+  public override async chatInputRun(interaction: CommandInteraction) {
+    return this.checkOwner(interaction.user.id);
+  }
+
+  public override async contextMenuRun(
+    interaction: ContextMenuCommandInteraction
+  ) {
+    return this.checkOwner(interaction.user.id);
+  }
+
+  private async checkOwner(userId: string) {
+    return userId === ownerId
+      ? this.ok()
+      : this.error({
+          message: "This command can only be used by the bot owner"
+        });
+  }
+}
