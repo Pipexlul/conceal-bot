3f7d816389a022ef8ca1c2f5802407ed5e15095f
Pipexlul <felipgg@gmail.com>
2023-08-24 03:11:56 -0400

    fix: client store loading issues
    Now, in all env modes, project is always started from dist/ folder using .js files.

--- src/classes/ConcealClient.ts
+++ src/classes/ConcealClient.ts
@@ -1,13 +1,8 @@
-import path from "path";
 import { SapphireClient } from "@sapphire/framework";
-import { getRootData } from "@sapphire/pieces";
 import type { ClientOptions } from "discord.js";
 import type { ConcealClientOptions } from "../interfaces/ConcealClientOptions";
 
-import { isDev } from "../utils/envUtils";
-
 class ConcealClient extends SapphireClient {
-  private rootData = getRootData();
   private customOptions?: ConcealClientOptions;
 
   public constructor(
@@ -15,12 +10,6 @@ class ConcealClient extends SapphireClient {
     options?: ConcealClientOptions
   ) {
     super(saphOptions);
-    // TODO: Change store paths here if needed
-    if (isDev) {
-      this.rootData.root = path.join(process.cwd(), "src");
-
-      console.log("Running in Dev mode, changed root to:", this.rootData.root);
-    }
 
     this.customOptions = options;
   }
