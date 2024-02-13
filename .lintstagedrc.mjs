604799178e7f7bf8441188b414a9a1ae893780d8
Pipexlul <felipgg@gmail.com>
2024-02-13 06:51:29 -0300

    chore: update and clean all dependencies. remove prettier&eslint in favor of biome

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,6 @@
+/** @type {import("lint-staged").Config} */
+const config = {
+  '*.{ts,js,mjs,cjs,json}': ['biome check --apply --no-errors-on-unmatched'],
+};
+
+export default config;
--- .prettierrc.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import("prettier").Config} */
-module.exports = {
-  trailingComma: "es5",
-  tabWidth: 2,
-  semi: true,
-  endOfLine: "lf",
-};
